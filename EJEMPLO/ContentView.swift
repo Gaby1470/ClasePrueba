bada22a836497adc3625c6f3aa17de2a685f4828
Gaby1470 <gabriela.sanchez.alcaraz@gmail.com>
2025-01-20 19:00:18 -0800

    Initial Commit

---
+++ EJEMPLO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EJEMPLO
+//
+//  Created by Gabriela Sanchez on 20/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EJEMPLO/EJEMPLOApp.swift
@@ -0,0 +1,17 @@
+//
+//  EJEMPLOApp.swift
+//  EJEMPLO
+//
+//  Created by Gabriela Sanchez on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct EJEMPLOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
