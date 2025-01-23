1a7bd90d958856be9d8b870972ab7609859717d0
GabrielaAlcaraz <gabriela.alcaraz@cetys.mx>
2025-01-22 16:38:47 -0800

    ejemplo

--- EJEMPLO/ContentView.swift
+++ EJEMPLO/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Circle()
+            
+            VStack(spacing: 40) {
+                HStack{
+                    Text("texto 1")
+                        .foregroundColor(.red)
+                    Text("texto 2")
+                        .foregroundColor(.red)
+                }
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+                    .padding(40)
+                    .background(.blue)
+                //            Spacer()
+                Text("adios huai ddjiao")
+            }
         }
-        .padding()
     }
 }
 
